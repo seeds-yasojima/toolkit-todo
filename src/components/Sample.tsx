51196ebd385818ff38d6c67c038e4aac217f05cb
fumiya yasojima <yasojima@seeds-std.co.jp>
2021-01-24 21:58:40 +0900

    setting

---
+++ src/components/Sample.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sample: React.FC = () => {
+  return (
+    <div>
+      <button className="button" onClick={() => console.log("button")}></button>
+    </div>
+  );
+};
+
+export default Sample;
